4c5a697ea3c8cd6968952442ef389c20223f61c0
Chintapakdumdum <48258553+chintapakdumdum22@users.noreply.github.com>
2024-10-14 22:07:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello from LuciferBanker'
 
-
 if __name__ == "__main__":
-    app.run()
+    # Ensure Flask listens on the correct port, in this case, 8080
+    port = int(os.getenv("PORT", 8080))  # Default to 8080 if no PORT is set
+    app.run(host="0.0.0.0", port=port)
